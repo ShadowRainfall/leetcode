7c1556770177c7d9678f1707a444c46bc182717e
ShadowRainfall <LuckyBullet.00@mail.ru>
2024-06-23 19:30:18 +0300

    modification of program

--- two_sum.py
+++ two_sum.py
@@ -1,4 +1,17 @@
-nt(input('Number of list elements: '))
-for i in range(0, length_m):
-        el_of_list = str(input('Your list: '))
-            list_m.append(el_of_list)
+nums = []
+length = int(input('Length of list elements: '))
+target = int(input("Input target: "))
+print("Target: ", target)
+index_first = 0
+index_second = 0
+if index_first in range(0, length):
+    for index_second in range(0, length):
+        element = int(input('Element: '))
+        nums.append(element)
+        if nums[index_first] + nums[index_second] == target:
+            index_first = index_first
+            index_second = index_second
+print("List: ", nums)
+print(f"Your indices: {index_first, index_second}")
+
+
