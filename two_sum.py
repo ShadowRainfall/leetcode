a74f903ae970478a345bb0cdbb5f1168ea790188
ShadowRainfall <LuckyBullet.00@mail.ru>
2024-06-23 20:24:06 +0300

    modification of program: adding search for all pairs

--- two_sum.py
+++ two_sum.py
@@ -1,17 +1,21 @@
 nums = []
-length = int(input('Length of list elements: '))
-target = int(input("Input target: "))
-print("Target: ", target)
+indices = []
+visited = set()
 index_first = 0
 index_second = 0
-if index_first in range(0, length):
-    for index_second in range(0, length):
-        element = int(input('Element: '))
-        nums.append(element)
-        if nums[index_first] + nums[index_second] == target:
-            index_first = index_first
-            index_second = index_second
+
+length = int(input('Length of list elements: '))
+target = int(input("Input target: "))
+for _ in range(length):
+    element = int(input('Element: '))
+    nums.append(element)
 print("List: ", nums)
-print(f"Your indices: {index_first, index_second}")
 
-
+for i in range(length):
+    for j in range(i + 1, length):
+        if nums[i] + nums[j] == target:
+            if (i, j) not in visited and (j, i) not in visited:
+                indices.append((i, j))
+                visited.add((i, j))
+                visited.add((j, i))
+print(f"Your indices: {indices}")
